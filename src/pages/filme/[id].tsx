8bba8e2c1d1a4ca484a9c82d8b39ff2093994301
LucasBargas <lucas.bargasoficial@gmail.com>
2023-01-08 23:24:34 -0300

    Movie page - fixed

--- src/pages/filme/[id].tsx
+++ src/pages/filme/[id].tsx
@@ -1,49 +1,34 @@
-import React from 'react';
-import { useRouter } from 'next/router';
 import HeadPerPage from '../../components/HeadPerPage';
 import MoviePage from '../../templates/MoviePage';
 import ISingleMovie from '../../interfaces/ISingleMovie';
+import { GetServerSideProps } from 'next';
+import { ParsedUrlQuery } from 'querystring';
 
 const apiUrl = process.env.NEXT_PUBLIC_API;
 const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 const apiImage = process.env.NEXT_PUBLIC_IMG;
 
-const Movie = () => {
-  const [movie, setMovie] = React.useState<ISingleMovie | null>(null);
-  const { query } = useRouter();
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const id = (context.params as ParsedUrlQuery).id;
+  const res = await fetch(`${apiUrl}/movie/${id}?${apiKey}&language=pt-BR`);
+  const movie = await res.json();
 
-  React.useEffect(() => {
-    const reqDatas = async () => {
-      let res;
-      let json;
+  return {
+    props: { movie },
+  };
+};
 
-      if (!query?.id) return;
+interface Props {
+  movie: ISingleMovie;
+}
 
-      try {
-        res = await fetch(
-          `${apiUrl}/movie/${query.id}?${apiKey}&language=pt-BR`,
-        );
-        json = await res.json();
-      } catch (error) {
-        json = null;
-        return error;
-      } finally {
-        setMovie(json);
-        return json;
-      }
-    };
-
-    reqDatas();
-  }, [query.id]);
-
+const Movie = ({ movie }: Props) => {
   const movieChanged = movie && {
     ...movie,
     poster: `${apiImage}${movie.poster_path}`,
   };
 
   if (!movieChanged) return;
-
-  // console.log(movieChanged);
 
   return (
     <>
